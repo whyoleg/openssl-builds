f14bdb271ff4c506e7830800b8569fc7458234f0
Oleg Yukhnevich <whyoleg@gmail.com>
2023-01-26 18:22:36 +0300

    initial build

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # openssl-builds
+
 Automated openssl builds
+
+```
+conan install packages/openssl3 --install-folder packages/openssl3/build/TARGET_REPLACE --build=missing --profile profiles/TARGET_REPLACE
+```
---
+++ packages/openssl3/conanfile.py
@@ -0,0 +1,11 @@
+from conans import ConanFile
+
+
+class Openssl3Conan(ConanFile):
+    def requirements(self):
+        self.requires("openssl/3.0.7")
+
+    def imports(self):
+        self.copy("libssl.a", dst="lib", src="lib")
+        self.copy("libcrypto.a", dst="lib", src="lib")
+        # self.copy("openssl/*.h", dst="include", src="include")
