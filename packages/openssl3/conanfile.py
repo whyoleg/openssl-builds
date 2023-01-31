be9d734e40014dafd0b52ddd864933e89ea1b0cf
Oleg Yukhnevich <whyoleg@gmail.com>
2023-01-31 14:36:49 +0300

    Revert "update imports"
    This reverts commit e5388de2617a3d11cbf0dff98eb69211f1ab1b3e.

--- packages/openssl3/conanfile.py
+++ packages/openssl3/conanfile.py
@@ -6,13 +6,5 @@ class Openssl3Conan(ConanFile):
         self.requires("openssl/3.0.7")
 
     def imports(self):
-        # static binary
-        self.copy("libcrypto.a", dst="lib", src="lib")
-        # windows dynamic binary
-        self.copy("libcrypto.dll.a", dst="lib", src="lib")
-        # linux dynamic binary
-        self.copy("libcrypto.so", dst="lib", src="lib")
-        # macos dynamic binary
-        self.copy("libcrypto.dylib", dst="lib", src="lib")
-        # headers
+        self.copy("libcrypto.*", dst="lib", src="lib")
         self.copy("openssl/*", dst="include", src="include")
