e5388de2617a3d11cbf0dff98eb69211f1ab1b3e
Oleg Yukhnevich <whyoleg@gmail.com>
2023-01-31 14:11:44 +0300

    update imports

--- packages/openssl3/conanfile.py
+++ packages/openssl3/conanfile.py
@@ -6,5 +6,13 @@ class Openssl3Conan(ConanFile):
         self.requires("openssl/3.0.7")
 
     def imports(self):
-        self.copy("libcrypto.*", dst="lib", src="lib")
+        # static binary
+        self.copy("libcrypto.a", dst="lib", src="lib")
+        # windows dynamic binary
+        self.copy("libcrypto.dll.a", dst="lib", src="lib")
+        # linux dynamic binary
+        self.copy("libcrypto.so", dst="lib", src="lib")
+        # macos dynamic binary
+        self.copy("libcrypto.dylib", dst="lib", src="lib")
+        # headers
         self.copy("openssl/*", dst="include", src="include")
