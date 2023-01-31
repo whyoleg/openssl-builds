9f24e99cc0267d2647ed14eb7e03e074f65e7b21
Oleg Yukhnevich <whyoleg@gmail.com>
2023-01-31 13:37:58 +0300

    upload only libcrypto.(a/dll/dylib/so)

--- packages/openssl3/conanfile.py
+++ packages/openssl3/conanfile.py
@@ -6,6 +6,5 @@ class Openssl3Conan(ConanFile):
         self.requires("openssl/3.0.7")
 
     def imports(self):
-        self.copy("*", dst="lib", src="lib")
-        # self.copy("libcrypto.a", dst="lib", src="lib")
-        # self.copy("openssl/*", dst="include", src="include")
+        self.copy("libcrypto.*", dst="lib", src="lib")
+        self.copy("openssl/*", dst="include", src="include")
