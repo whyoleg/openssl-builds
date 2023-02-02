b34a4e7046fd93aebdae4e8dc42548ecf6dc60be
Oleg Yukhnevich <whyoleg@gmail.com>
2023-02-02 15:51:24 +0300

    split dynamic and static dont upload per target for now

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # openssl-builds
 
 Automated openssl builds
-
-```
-conan install packages/openssl3 --install-folder build/openssl3/TARGET_REPLACE --build=missing --profile profiles/TARGET_REPLACE
-```
--- packages/openssl3/conanfile.py
+++ packages/openssl3/conanfile.py
@@ -6,5 +6,12 @@ class Openssl3Conan(ConanFile):
         self.requires("openssl/3.0.7")
 
     def imports(self):
-        self.copy("libcrypto.*", dst="lib", src="lib")
+        self.copy("libcrypto.dll.a", dst="lib/dynamic", src="lib")
+        self.copy("libcrypto.3.dylib", dst="lib/dynamic", src="lib")
+        self.copy("libcrypto.dylib", dst="lib/dynamic", src="lib")
+        self.copy("libcrypto.so.3", dst="lib/dynamic", src="lib")
+        self.copy("libcrypto.so", dst="lib/dynamic", src="lib")
+
+        self.copy("libcrypto.a", dst="lib/static", src="lib")
+
         self.copy("openssl/*", dst="include", src="include")
