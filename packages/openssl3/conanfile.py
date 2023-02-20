23d6eae39c22d2fe35dee8c4621146af613c2cf5
Oleg Yukhnevich <whyoleg@gmail.com>
2023-02-20 21:34:13 +0300

    windows support change libraries paths

--- packages/openssl3/conanfile.py
+++ packages/openssl3/conanfile.py
@@ -6,5 +6,15 @@ class Openssl3Conan(ConanFile):
         self.requires("openssl/3.0.8")
 
     def imports(self):
-        self.copy("*", dst="lib", src="lib")
-        self.copy("openssl/*", dst="include", src="include")
+        # headers
+        self.copy("*", dst="include", src="include")
+        # Windows libraries
+        self.copy("*.dll", dst="lib", src="bin")
+        self.copy("*.lib", dst="lib", src="lib")
+        # static libraries ( + dynamic mingw) libraries
+        self.copy("*.a", dst="lib", src="lib")
+        # macos dynamic libraries
+        self.copy("*.dylib", dst="lib", src="lib")
+        # linux/android dynamic libraries
+        self.copy("*.so", dst="lib", src="lib")
+        self.copy("*.so.3", dst="lib", src="lib")
