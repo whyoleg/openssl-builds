881829b5ea59396b1c63134b7457849582ef403d
Oleg Yukhnevich <whyoleg@gmail.com>
2023-02-20 18:01:45 +0300

    wasm and android builds

--- packages/openssl3/conanfile.py
+++ packages/openssl3/conanfile.py
@@ -6,20 +6,5 @@ class Openssl3Conan(ConanFile):
         self.requires("openssl/3.0.8")
 
     def imports(self):
-        self.copy("libcrypto.dll.a", dst="lib/dynamic", src="lib")
-        self.copy("libcrypto.3.dylib", dst="lib/dynamic", src="lib")
-        self.copy("libcrypto.dylib", dst="lib/dynamic", src="lib")
-        self.copy("libcrypto.so.3", dst="lib/dynamic", src="lib")
-        self.copy("libcrypto.so", dst="lib/dynamic", src="lib")
-
-        self.copy("libcrypto.a", dst="lib/static", src="lib")
-
-        self.copy("libssl.dll.a", dst="lib/dynamic", src="lib")
-        self.copy("libssl.3.dylib", dst="lib/dynamic", src="lib")
-        self.copy("libssl.dylib", dst="lib/dynamic", src="lib")
-        self.copy("libssl.so.3", dst="lib/dynamic", src="lib")
-        self.copy("libssl.so", dst="lib/dynamic", src="lib")
-
-        self.copy("libssl.a", dst="lib/static", src="lib")
-
+        self.copy("*", dst="lib", src="lib")
         self.copy("openssl/*", dst="include", src="include")
