90d4919c56d22897e1f5ef82178b877a3c703db9
Oleg Yukhnevich <whyoleg@gmail.com>
2023-07-25 10:23:54 +0300

    test all

--- packages/openssl3/conanfile.py
+++ packages/openssl3/conanfile.py
@@ -9,25 +9,13 @@ class Openssl3Conan(ConanFile):
         self.requires("openssl/3.1.1")
 
     def generate(self):
-        for dep in self.dependencies.values():
-            copy(self, "*.h", dep.cpp_info.includedirs[0], join(self.build_folder, "include"))
-            copy(self, "*.dll", dep.cpp_info.libdirs[0], join(self.build_folder, "lib"))
-            copy(self, "*.lib", dep.cpp_info.libdirs[0], join(self.build_folder, "lib"))
-            copy(self, "*.a", dep.cpp_info.libdirs[0], join(self.build_folder, "lib"))
-            copy(self, "*.dylib", dep.cpp_info.libdirs[0], join(self.build_folder, "lib"))
-            copy(self, "*.so", dep.cpp_info.libdirs[0], join(self.build_folder, "lib"))
-            copy(self, "*.so.3", dep.cpp_info.libdirs[0], join(self.build_folder, "lib"))
-
-    # def imports(self):
-    #     # headers
-    #     self.copy("*", dst="include", src="include")
-    #     # Windows libraries
-    #     self.copy("*.dll", dst="lib", src="bin")
-    #     self.copy("*.lib", dst="lib", src="lib")
-    #     # static libraries ( + dynamic mingw) libraries
-    #     self.copy("*.a", dst="lib", src="lib")
-    #     # macos dynamic libraries
-    #     self.copy("*.dylib", dst="lib", src="lib")
-    #     # linux/android dynamic libraries
-    #     self.copy("*.so", dst="lib", src="lib")
-    #     self.copy("*.so.3", dst="lib", src="lib")
+        for dep in [self.dependencies["openssl"], self.dependencies["zlib"]]:
+            includedir = dep.cpp_info.includedirs[0]
+            libdir = dep.cpp_info.libdirs[0]
+            copy(self, "*.h", includedir, join(self.build_folder, "include"))
+            copy(self, "*.dll", libdir, join(self.build_folder, "lib"))
+            copy(self, "*.lib", libdir, join(self.build_folder, "lib"))
+            copy(self, "*.a", libdir, join(self.build_folder, "lib"))
+            copy(self, "*.dylib", libdir, join(self.build_folder, "lib"))
+            copy(self, "*.so", libdir, join(self.build_folder, "lib"))
+            copy(self, "*.so.3", libdir, join(self.build_folder, "lib"))
